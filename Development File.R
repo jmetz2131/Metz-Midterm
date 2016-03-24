5ea40adea519fd1f180ac455eb52947411e9c2c1
jmetz2131 <jacob.metz@wustl.edu>
2016-03-23 23:37:45 -0500

    Created the Basic integrateIt Package
    Though the methods I’ve written and the integrateIt method is still
    incomplete as of this point, I successfully created the package
    integrateIt and included a Development File. Additionally, I changed
    the DESCRIPTION file to properly convey what the package is supposed to
    do.

---
+++ Development File.R
@@ -0,0 +1,14 @@
+###Here I am creating a new development file for the package integrateIt
+##I downloaded devtools and roxygen2 in order to make working with the class systems and methods
+##I've already created easier
+library(devtools)
+library(roxygen2)
+
+##Here I am verifying the proper working directory
+getwd()
+setwd("/Users/jacobmetz/Desktop/New Class/Metz-Midterm")
+
+package.skeleton(name = "integrateIt")
+
+current.code <- as.package("primaryrace")
+load_all(current.code)
